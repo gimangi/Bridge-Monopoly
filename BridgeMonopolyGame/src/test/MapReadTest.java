759d511ab096144dcaa8f705d6448074e6394a57
gimangi <khk211113@naver.com>
2022-06-04 17:17:53 +0900

    Add map read test

--- BridgeMonopolyGame/src/test/MapReadTest.java
+++ BridgeMonopolyGame/src/test/MapReadTest.java
@@ -1,42 +1,32 @@
 package test;
 
-import model.data.Direction;
-import model.domain.cell.Cell;
 import model.domain.map.Map;
-import model.domain.map.MapDecoder;
-import model.domain.player.Player;
+import model.domain.map.MapReader;
 
-import java.util.ArrayList;
-import java.util.List;
+public class MapReadTest {
 
-import static model.data.Direction.*;
-import static model.data.MoveType.ADJACENT;
-import static model.data.MoveType.FORWARD;
+    private static final String[] TEST_MAPS = { "default.map", "another.map", "maze.map", "snake.map" };
 
-public class DomainTest {
     public static void main(String args[]) {
-        try {
-            MapDecoder decoder = new MapDecoder();
-            Map map = decoder.getBoard();
-            Cell startCell = map.getStartCell();
 
-            testMapMove(startCell);
+        int cnt = 0;
+        for (String name : TEST_MAPS) {
+            try {
+                MapReader reader = new MapReader(name);
+                Map map = reader.getMap();
 
-            map.createAbsoluteMap();
-            map.printAbsoluteMap();
+                map.createAbsoluteMap();
 
-        } catch (Exception e) {
-            e.printStackTrace();
+                System.out.println("Map test pass " + ++cnt + " / " + TEST_MAPS.length);
+            } catch (Exception e) {
+                System.out.println("Map test failed at " + name);
+                e.printStackTrace();
+            }
         }
+
+        if (cnt == TEST_MAPS.length)
+            System.out.println("All tests passed successfully.");
+
     }
 
-    private static void testMapMove(Cell startCell) {
-        ArrayList<Direction> dirs = new ArrayList<>();
-        dirs.addAll(List.of(new Direction[]{RIGHT, RIGHT, DOWN, RIGHT, RIGHT}));
-
-        Player player = Player.newInstance(startCell);
-
-        System.out.println(player.move(dirs, ADJACENT));
-        System.out.println(player.move(dirs, FORWARD));
-    }
 }
